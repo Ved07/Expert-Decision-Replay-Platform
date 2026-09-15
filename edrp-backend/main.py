ad5ba38d356d8c1fefae998fdf0a47949aab891e
Ved07 <vedansh.mudgal25@gmail.com>
2026-09-16 00:00:36 +0530

    Fix CORS issue

--- edrp-backend/main.py
+++ edrp-backend/main.py
@@ -21,7 +21,10 @@ app = FastAPI(title="Expert Decision Replay Platform API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://expert-decision-replay-platform-frontend.onrender.com"
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
